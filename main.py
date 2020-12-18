8d7788a0592314b2b9bc8a95bdecf6a3db36a97e
kamigo777 <64247014+kamigo777@users.noreply.github.com>
2020-12-18 13:57:31 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Это игра по угадванию чисел
+import random
+
+guessesTaken = 0
+
+print('Привет! Как тебя зовут?')
+myName = input()
+
+number = random.randint(1, 20)
+print('Что ж, ' + myName + ' , я загадываю число от 1 до 20.')
+
+for guessesTaken in range(6):
+    print('Попробуй угадать.')
+    guess = input()
+    guess = int(guess)
+
+    if guess < number:
+        print('Твое число слишком маленькое.')
+
+    if guess > number:
+        print ('Твое число сшком большое.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken + 1)
+    print ('Отлично ' +myName+ '! Ты справился ' +guessesTaken + 'попытки!')
